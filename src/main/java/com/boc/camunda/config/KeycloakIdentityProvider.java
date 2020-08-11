90ecaf0e2fd34d888956915e21423a2d8ff16fe2
cx <cx810275221@163.com>
2020-08-11 14:17:37 +0800

    camunda

---
+++ src/main/java/com/boc/camunda/config/KeycloakIdentityProvider.java
@@ -0,0 +1,12 @@
+//package com.boc.camunda.config;
+//
+//import org.camunda.bpm.extension.keycloak.plugin.KeycloakIdentityProviderPlugin;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.context.annotation.PropertySource;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@ConfigurationProperties(prefix="plugin.identity.keycloak")
+//@PropertySource("classpath:application.yml")
+//public class KeycloakIdentityProvider extends KeycloakIdentityProviderPlugin {
+//}
